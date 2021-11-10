4a3d0522ca561b376684ee794b0dde5883f4e0d0
Murashkina <grewytri33@gmail.com>
2021-11-10 08:27:14 +0400

    Чтение файла

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,6 +1,9 @@
-public class Matrix{
-    int numberofrows;
-    int numberofcolumns;
+import java.io.*;
+import java.util.Scanner;
+
+public class Matrix {
+    private int numberofrows;
+    private int numberofcolumns;
     private double[][] matrix;
 
     public double[][] getMarix() {
@@ -18,13 +21,77 @@ public class Matrix{
     }
     //два метода возр кол-во строк и кол-во столбцов соотв
     public int GetNuberofColumns() {
-        int nosCols = matrix.length;
-        return nosCols;
+        return numberofcolumns;
     }
 
     public int GetNuberofRows() {
-        int nosRows = matrix[0].length;
-        return nosRows;
+        return numberofrows;
+    }
+    public void setElements(int a, int b, double newValue) {
+
+        matrix[a][b] = newValue;
+    }
+    public double getElements(int a, int b) {//метод реализует отдельное добавление
+
+        return matrix [a][b];
+    }
+    public static double[][] MatrixMulti(double[][] MatrixA,double[][] MatrixB) {//умножение матриц
+        int m = MatrixA.length;
+        int n = MatrixB[0].length;
+        int o = MatrixB.length;
+        double[][] result = new double[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < o; k++) {
+                    result[i][j] += MatrixA[i][k] * MatrixB[k][j];
+                    return result;
+                }
+            }
+        }
+        return result;
     }
 
+    public static void writeToFile(FileWriter fileWriter, Matrix matrix) {
+        try {
+            fileWriter.write(matrix.numberofrows() + "\n");
+            fileWriter.write(matrix.numberofcolumns() + "\n");
+            fileWriter.write(matrix.toString());
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+    public static void writeMatrix(double [][] matrix) throws IOException {
+
+
+        try {
+            OutputStream output = new FileOutputStream("file.txt"); // Создание текстового файла
+            for(int j=0;j<matrix[0].length;j++) {
+                for (int i = 0; i < matrix.length; i++) {
+                    output.write(matrix[i][j]); // Запись каждого символа в текстовый файл
+                }
+            }
+            output.close();
+
+            InputStream input = new FileInputStream("file.txt");
+            int size = input.available();
+
+            for(int j = 0; j < size; j++) {
+                System.out.print((char)input.read() + " "); // Чтение текстового файла посимвольно
+            }
+            input.close();
+
+        }catch(IOException e) {
+            System.out.print("Exception");
+        }
+    }
 }
+
